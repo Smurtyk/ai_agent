74eab293d1eaba7053615c89a4624ff0bbef20d3
Smurtyk <Bnenad98@gmail.com>
2025-06-22 23:20:38 +0200

    Updated system prompt

--- prompts.py
+++ prompts.py
@@ -10,6 +10,9 @@ Do not make guesses or output explanations based on assumptions. You can perform
 - Write or overwrite files using the write_file function
 
 Make a plan for which functions to call, then call them in sequence to gather the required information.
+Most of your plans should start by scanning the working directory (`.`) for relevant files and directories. Don't ask me where the code is, go look for it with your list tool.
+
+Execute code of the application itself when you're done making modifications to ensure that everything works as expected.
 Only after confirming you have accomplished the task, provide a final response describing what was done. If you cannot complete the request, explain why.
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls because it is automatically injected for security reasons.
